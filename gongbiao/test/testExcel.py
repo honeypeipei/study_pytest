44657910712c405a93ee3adef3fad4375e7b8305
honeypeipei <478313194@qq.com>
2020-08-14 19:16:11 +0800

    修改标准号完成

--- gongbiao/test/testExcel.py
+++ gongbiao/test/testExcel.py
@@ -19,7 +19,7 @@ def excel():
         #因为表内可能存在多列数据，0代表第一列数据，1代表第二列，以此类推
         if data != '':
             mydata = data[0:data.find('-')] + data[data.find('-'):((data.find('-'))+5)]
-            dat.append(mydata)
+            dat.append(mydata.strip())
         #把每次循环读取的数据插入到list
     # return dat
     res_dat =  []
@@ -33,3 +33,4 @@ def res(par):   #a变量传入
         print(i)
 
 res(excel())
+print(len(excel()))
